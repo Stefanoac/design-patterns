8eccec27e443cc9eed48d3ed522fc065d9ceb6b4
Stéfano Caberlon <stefanocaberlon5@gmail.com>
2021-06-07 11:16:32 -0300

    Add factory method pattern

---
+++ DesignPatterns/FactoryMethod/Factories/CarTransport.cs
@@ -0,0 +1,13 @@
+﻿using FactoryMethod.Vehicles;
+using System;
+
+namespace FactoryMethod.Factories
+{
+    class CarTransport : Transport
+    {
+        protected override IVehicle CreateTransport()
+        {
+            return new Car();
+        }
+    }
+}
---
+++ DesignPatterns/FactoryMethod/Vehicles/Bike.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryMethod.Vehicles
+{
+    class Bike : IVehicle
+    {
+        public void GetCargo()
+        {
+            Console.WriteLine("Já pegamos a comida!");
+        }
+
+        public void StartRoute()
+        {
+            Console.WriteLine("Iniciando o delivery.");
+            GetCargo();
+        }
+    }
+}
